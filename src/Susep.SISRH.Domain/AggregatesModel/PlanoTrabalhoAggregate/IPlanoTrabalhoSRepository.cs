820e06418ae4e2db2a9ff5c46af22ab6e85e87ad
Luísa Behrens Palmeira <lubpalmeira@gmail.com>
2023-06-26 16:39:19 -0300

    Create IPlanoTrabalhoSRepository.cs

---
+++ src/Susep.SISRH.Domain/AggregatesModel/PlanoTrabalhoAggregate/IPlanoTrabalhoSRepository.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Susep.SISRH.Domain.AggregatesModel.PlanoTrabalhoAggregate
+{
+    public interface IPlanoTrabalhoSRepository
+    {
+        Task<PlanoTrabalho> ObterAsync(Guid planoTrabalhoId);
+
+        Task<PlanoTrabalho> AdicionarAsync(PlanoTrabalho item);
+
+        void Atualizar(PlanoTrabalho item);
+
+        void Excluir(PlanoTrabalho item);
+    }
+}
